218ba597fb00b66110427dbd9cbd978d000cace9
janiejestemja <janiejestemja@web.de>
2025-03-06 00:04:46 +0100

    moved functions into utils.py

--- main.py
+++ main.py
@@ -1,132 +1,20 @@
 import sys
-from aes.pyaes import key_expansion, aes_encrypt, aes_decrypt
+
+from utils import load_files, save_files
 
 def main():
+    if len(sys.argv) != 4:
+        sys.exit("Missing cla")
 
+    filename = sys.argv[2]
+    cryptname = sys.argv[3]
 
     if sys.argv[1] == "-en":
-        save_files()
+        save_files(filename=filename, cryptname=cryptname)
+
     elif sys.argv[1] == "-de":
-        load_files()
-
-def save_files():
-    print("Sourcetext: ")
-    print(read_text())   
-
-    key = bytes( 
-        [0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f]
-    )
-    round_keys = key_expansion(key)   
-
-    # Encryption
-    states = str_to_states(read_text())
-    cipherstates = []
-    for state in states:
-        cipherstates.append(aes_encrypt(state, round_keys))
-
-    # Save encrypted file
-    save_crypt("crypt.txt", cipherstates)
-
-    with open("key.txt", "w") as f:
-        for ele in round_keys:
-            f.write(str(ele) + "\n")
-
-def load_files():
-    loaded_keys = []
-    with open("key.txt") as f:
-        reader = f.readlines()
-        loaded_keys = [int(key.strip("\n")) for key in reader]
-    # Load encrypted file
-    text_fromf = read_crypt("crypt.txt")
-
-    # Decryption
-    decipheredstates = [aes_decrypt(state, loaded_keys) for state in text_fromf]
-
-    print("Deciphered Text: ")
-    print(states_to_text(decipheredstates))
-
-def read_text(filename="lore.txt"):
-    text = ""
-    with open(filename) as f:
-        reader = f.read()
-        text = reader
-
-    return text
-
-def save_crypt(filename, cipherstates):
-    state_strs = []
-    for state in cipherstates:
-        row_strs = []
-        for row in state:
-            row_str = ",".join(str(ele) for ele in row)
-            row_strs.append(row_str)
-        state_str = ";".join(row for row in row_strs)
-        state_strs.append(state_str)
-
-    with open(filename, "w") as f:
-        for state_str in state_strs:
-            f.write(state_str + "\n")
-
-def read_crypt(filename):
-    # Reading from file
-    from_file = []
-    with open(filename) as f:
-        reader = f.readlines()
-        for line in reader:
-            from_file.append(line.strip("\n"))
-
-    text_fromf = []
-    for line in from_file:
-        row_strs = line.split(";")
-
-        state = []
-        for row_str in row_strs:
-            row = [int(ele) for ele in row_str.split(",")]
-            state.append(row)
-        text_fromf.append(state)
-
-    return text_fromf
-
-
-def str_to_states(text : str) -> list:
-    const = 16
-    hex_text = []
-
-    for char in text:
-        if ord(char) < 0 or ord(char) > 254:
-            hex_text.append(hex(ord("?")))
-        else:
-            hex_text.append(hex(ord(char)))
-            
-    for _ in range(const - (len(text) % const)):
-        hex_text.append(hex(ord(" ")))
-        
-    states = []
-
-    hex_len = len(hex_text)
-    
-    for i in range(0, hex_len, 16):
-        state = []
-        
-        for j in range(4):
-            row = []
-            
-            for k in range(4):
-                row.append(int(hex_text[i + 4 * j + k], 16))
-            state.append(row)
-        states.append(state)
-
-    return states
-
-def states_to_text(states : list) -> str:
-    txt_from_hex = ""
-    
-    for i, state in enumerate(states):
-        for j, row in enumerate(state):
-            for k, element in enumerate(row):
-                txt_from_hex = txt_from_hex + chr(int(states[i][j][k]))
-
-    return txt_from_hex
+        with open(filename, "w") as f:
+            f.write(load_files(cryptname=cryptname))
 
 if __name__ == "__main__":
     main()
