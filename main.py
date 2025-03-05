c632d87bdfbd8af6f6be8d10cfb38f9e5a7a3f01
janiejestemja <janiejestemja@web.de>
2025-03-06 00:28:38 +0100

    extended main.py

--- main.py
+++ main.py
@@ -6,13 +6,15 @@ def main():
     if len(sys.argv) != 4:
         sys.exit("Missing cla")
 
-    filename = sys.argv[2]
-    cryptname = sys.argv[3]
 
     if sys.argv[1] == "-en":
+        filename = sys.argv[2]
+        cryptname = sys.argv[3]
         save_files(filename=filename, cryptname=cryptname)
 
     elif sys.argv[1] == "-de":
+        cryptname = sys.argv[2]
+        filename = sys.argv[3]
         with open(filename, "w") as f:
             f.write(load_files(cryptname=cryptname))
 
