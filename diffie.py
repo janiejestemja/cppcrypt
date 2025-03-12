04c470f037cdfe80d3ff07bb234ec3edc2bf66e8
janiejestemja <janiejestemja@web.de>
2025-03-12 13:09:24 +0100

    sketched out diffie-hellman key-exchange

---
+++ diffie.py
@@ -0,0 +1,84 @@
+import socket
+import sys
+from time import sleep
+
+def main():
+    if sys.argv[1] == "h":
+        server()
+    elif sys.argv[1] == "c":
+        client()
+
+def generate_public():
+    prime = 23
+    prim_root = 5
+    a = 4
+
+    big_a = pow(prim_root, a)
+
+    return (a, big_a, prime, prim_root)
+
+def client_secret(big_a, prime, prim_root):
+    b = 8
+    big_b = pow(prim_root, b)
+
+    shared_secret = pow(big_a, b) % prime
+
+    return (b, big_b, shared_secret)
+
+def server_secret(a, big_b, prime):
+    shared_secret = pow(big_b, a) % prime
+
+    return shared_secret
+
+def server():
+    a, big_a, prime, prim_root = generate_public()
+    print(a, big_a, prime, prim_root, sep= " : ")
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.bind(("127.0.0.1", 12345))
+    server_socket.listen()
+    print("Server is listening...")
+
+    while True:
+        client_socket, addr = server_socket.accept()
+        print(f"Connection from {addr}")
+
+        # Send public variables to client
+        client_socket.send(bytes(str(big_a).encode()))
+        sleep(0.1)
+        client_socket.send(bytes(str(prime).encode()))
+        sleep(0.1)
+        client_socket.send(bytes(str(prim_root).encode()))
+        sleep(0.1)
+        client_socket.send(bytes(str("exit()").encode()))
+        print("Public variables send")
+
+        big_b = int(client_socket.recv(1024).decode())
+        shared_secret = server_secret(a, big_b, prime)
+        print(shared_secret)
+        client_socket.close()
+        server_socket.close()
+        break
+
+def client():
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket.connect(("127.0.0.1", 12345))
+
+    # Recieve public variables from server
+    public_variables = []
+    while True:
+        message = client_socket.recv(1024)
+        if message.decode() == "exit()":
+            break
+        else:
+            public_variables.append(int(message.decode()))
+        print(f"Recieved: {message.decode()}")
+
+
+    b, big_b, shared_secret = client_secret(*public_variables)
+    client_socket.send(bytes(str(big_b).encode()))
+    print(shared_secret)
+
+    client_socket.close()
+
+if __name__ == "__main__":
+    main()
