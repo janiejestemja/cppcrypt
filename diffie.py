7e91841682421d2e2aac97339178b3005e3d1186
janiejestemja <janiejestemja@web.de>
2025-03-13 04:46:49 +0100

    extended diffie.py

--- diffie.py
+++ diffie.py
@@ -5,6 +5,7 @@ from random import choice, randint
 
 from prim_roots import primitive_roots as pr
 from aes.pyaes import key_expansion, aes_encrypt, aes_decrypt
+from utils import states_to_text, str_to_states, read_text
 
 def main():
     if sys.argv[1] == "h":
@@ -12,12 +13,6 @@ def main():
     elif sys.argv[1] == "c":
         client()
 
-state = [
-        [0x00, 0x01, 0x02, 0x03],
-        [0x10, 0x11, 0x12, 0x13],
-        [0x20, 0x21, 0x22, 0x23],
-        [0x30, 0x31, 0x32, 0x33]
-]
 
 primes = [
         2015121110987654321,
@@ -25,6 +20,8 @@ primes = [
         7393913311133193937,
         8888888897888888899,
 ]
+
+text = read_text()
 
 def generate_public():
     prime = primes[0]
@@ -49,8 +46,14 @@ def server_secret(a, big_b, prime):
     return shared_secret
 
 def server():
+    state = [
+            [0x00, 0x01, 0x02, 0x03],
+            [0x10, 0x11, 0x12, 0x13],
+            [0x20, 0x21, 0x22, 0x23],
+            [0x30, 0x31, 0x32, 0x33]
+    ]
     a, big_a, prime, prim_root = generate_public()
-    print(a, big_a, prime, prim_root, sep= " : ")
+    print(big_a, prime, prim_root, sep= " : ")
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_socket.bind(("127.0.0.1", 12345))
     server_socket.listen()
@@ -70,25 +73,32 @@ def server():
         client_socket.send(bytes(str("exit()").encode()))
         print("Public variables send")
 
+        # Recieve public variable
         big_b = int(client_socket.recv(1024).decode())
+        # Calculate shared secret
         shared_secret = server_secret(a, big_b, prime)
         print(shared_secret)
 
+        # Expand Key
         round_keys = key_expansion(bytes(str(shared_secret)[:16].encode()))
+        # Encrypt one state
         cipherstate = aes_encrypt(state, round_keys)
-
+        # Send states elements
         for row in cipherstate:
             for ele in row:
                 client_socket.send(bytes(str(ele).encode()))
                 sleep(0.1)
-
         client_socket.send(bytes(str("exit()").encode()))
         sleep(0.1)
 
-        print(round_keys)
-
-        for row in state:
-            print(row)
+        states = str_to_states(text)
+        cipherstates = [aes_encrypt(state, round_keys) for state in states]
+        for cipherstate in cipherstates:
+            for row in cipherstate:
+                for ele in row:
+                    client_socket.send(bytes(str(ele).encode()))
+                    sleep(0.1)
+        client_socket.send(bytes(str("exit()").encode()))
 
         client_socket.close()
         server_socket.close()
@@ -109,10 +119,15 @@ def client():
         print(f"Recieved: {message.decode()}")
 
 
+    # Calculate private var, public var and shared secret
     b, big_b, shared_secret = client_secret(*public_variables)
-    client_socket.send(bytes(str(big_b).encode()))
     print(shared_secret)
+    round_keys = key_expansion(bytes(str(shared_secret)[:16].encode()))
 
+    # Send public var
+    client_socket.send(bytes(str(big_b).encode()))
+
+    # Recieve one ciphered state
     ciphers = []
     while True:
         message = client_socket.recv(1024)
@@ -121,6 +136,7 @@ def client():
         else:
             ciphers.append(int(message.decode()))
 
+    # Reformat recieved ciphered state
     cipherstate = []
     for i in range(4):
         row = []
@@ -128,12 +144,36 @@ def client():
             row.append(ciphers[4 * i + j])
         cipherstate.append(row)
 
-    round_keys = key_expansion(bytes(str(shared_secret)[:16].encode()))
-    print(round_keys)
-
+    # Decipher state
     deciphered_state = aes_decrypt(cipherstate, round_keys)
-    for row in deciphered_state:
-        print(row)
+
+    # Decipher text
+    i = 0
+    j = 0
+    row = []
+    cipherstate = []
+    cipherstates = []
+    while True:
+        message = client_socket.recv(1024)
+        if message.decode() == "exit()":
+            break
+        elif message.decode() == "":
+            continue
+        else:
+            row.append(int(message.decode()))
+            j += 1
+            if j > 3:
+                cipherstate.append(row)
+                i += 1
+                row = []
+                j = 0
+            if i > 3:
+                cipherstates.append(cipherstate)
+                cipherstate = []
+                i = 0
+
+    states = [aes_decrypt(cipherstate, round_keys) for cipherstate in cipherstates]
+    print(states_to_text(states))
 
     client_socket.close()
 
