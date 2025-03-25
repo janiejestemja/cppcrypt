b573213d40fa0723e77908e9e53665164036b2fa
janiejestemja <janiejestemja@web.de>
2025-03-25 10:10:42 +0100

    removed unused code

--- ecdiffify.py
+++ ecdiffify.py
@@ -33,58 +33,5 @@ def main():
     elif sys.argv[1] == "c":
         client(ipvfour, portnumber)
 
-def validate(ip):
-    """ 
-    Simplified validation of IPv4 Adresses.
-
-    Args:
-        ip (str): A str potentially containing valid IPv4 Adress.
-    
-    Returns:
-        bool: Result of validation.
-    """
-
-    # trying to extract substrings into a list
-    try: 
-        elements = list(ip.split("."))
-
-    # handling exception
-    except (ValueError, TypeError):
-        return False
-
-    else: 
-        # trying to typeforce elements in the list
-        try:
-            numb3rs = list()
-
-            for element in elements:
-                numb3rs.append(int(element))
-
-        # handling exception
-        except ValueError:
-            return False 
-
-        else:
-            # checking for correct length
-            if len(numb3rs) == 4:
-                # Current state
-                state = True
-                # checking accepted ranges for numbers in the adress
-                for numb3r in numb3rs:
-                    if numb3r > 255:
-                        state = False
-
-                    elif numb3r < 0:
-                        state = False
-
-                    else:
-                        continue
-
-                return state
-
-            else:
-                # otherwise returning false
-                return False
-
 if __name__ == "__main__":
     main()
