02309c74f6e30db424c68e422e888fa6a6dddc2d
janiejestemja <janiejestemja@web.de>
2025-04-01 23:38:31 +0200

    added save feature

--- runner.py
+++ runner.py
@@ -1,10 +1,13 @@
+import os
+
 import tkinter as tk
 from tkinter import filedialog
 
-from cryptology.utils import check_passkey, load_files
+from cryptology.utils import check_passkey, load_file, save_file
 
 infile_path = ""
 default_passkey = "0,1,2,3,4,5,6,7,8,9,a,b,c,d,e,f"
+outfile_defaultname = "crypt.txt"
 
 def main():
     print("Starting app...")
@@ -14,27 +17,39 @@ def main():
     root.title("Tkinter GUI")
     root.geometry("1280x800")
 
-    # Navigation bar 
-    navbar = tk.Frame(root)
-    navbar.pack(side="top", padx=10, pady=10, fill="both")
+    # Actions
+    actions_frame = tk.Frame(root)
+    actions_frame.pack(side="top", padx=10, pady=10, fill="both")
 
     # Open button for file
-    choose_file_button = tk.Button(navbar, text="Open File", command=lambda: choose_file(text_area))
+    choose_file_button = tk.Button(actions_frame, text="Open File", command=lambda: choose_infile(text_area))
     choose_file_button.pack(side="left")
 
-    # Textinput for passkey
-    pw_entry = tk.Entry(navbar, width=32)
-    pw_entry.pack(side="left")
-
     # Encryption button
-    encrypt_btn = tk.Button(navbar, text="Encrypt", command=lambda: encrypt_txt(pw_entry))
+    encrypt_btn = tk.Button(actions_frame, text="Encrypt opened File", command=lambda: encrypt_txt(pw_entry))
     encrypt_btn.pack(side="left")
 
-    # Decryption button
-    decrypt_btn = tk.Button(navbar, text="Decrypt", command=lambda: decrypt_txt(pw_entry, text_area))
+     # Decryption button
+    decrypt_btn = tk.Button(actions_frame, text="Decrypt Text", command=lambda: decrypt_txt(pw_entry, text_area))
     decrypt_btn.pack(side="left")
 
-    # Create frame for scrollbar and text
+   # Save button
+    save_btn = tk.Button(actions_frame, text="Save Text", command=lambda: save_txt(text_area))
+    save_btn.pack(side="left")
+
+    # Frame for passkey
+    passkey_frame = tk.Frame(root)
+    passkey_frame.pack(side="top", padx=10, pady=10, fill="both")
+
+    # Label for passkey input
+    pw_label = tk.Label(passkey_frame, text="Passkey: ")
+    pw_label.pack(side="left")
+
+    # Textinput for passkey
+    pw_entry = tk.Entry(passkey_frame, width=32)
+    pw_entry.pack(side="left")
+
+    # Frame for Textfile
     frame = tk.Frame(root)
     frame.pack(side="top", padx=10, pady=10, fill="both")
 
@@ -53,7 +68,22 @@ def main():
 
 # Helperfunctions
 def encrypt_txt(pw_entry):
-    print("Not implemented")
+    passkey = pw_entry.get()
+
+    if passkey == "":
+        passkey = default_passkey
+
+    if check_passkey(passkey) == False:
+        print("Passkey check failed")
+        return None
+
+    file_path = filedialog.asksaveasfilename(defaultextension=".txt", filetypes=[("Text Files", "*.txt")])
+
+    if file_path:
+        infile_name = os.path.basename(infile_path)
+        outfile_name = os.path.basename(file_path)
+        save_file(file_name=infile_name, crypt_name=outfile_name, passkey=passkey)
+        print("File saved")
 
 def decrypt_txt(pw_entry, text_area):
     passkey = pw_entry.get()
@@ -67,10 +97,18 @@ def decrypt_txt(pw_entry, text_area):
 
     if infile_path != "":
         text_area.delete("1.0", tk.END)
-        text_area.insert(tk.END, load_files(crypt_name=infile_path, passkey=passkey))
+        text_area.insert(tk.END, load_file(crypt_name=infile_path, passkey=passkey))
+
+def save_txt(text_area):
+    file_path = filedialog.asksaveasfilename(defaultextension=".txt", filetypes=[("Text Files", "*.txt")])
+    if file_path:
+        with open(file_path, "w") as f:
+            f.write(text_area.get("1.0", "end-1c"))
+        
+        print("File saved")
 
 # Display a textile
-def choose_file(text_area):
+def choose_infile(text_area):
     global infile_path
 
     file_path = filedialog.askopenfilename(filetypes=[("Text files", "*.txt")])
@@ -80,7 +118,6 @@ def choose_file(text_area):
             text_area.insert(tk.END, f.read())
 
         infile_path = file_path
-
 
 if __name__ == "__main__":
     main()
